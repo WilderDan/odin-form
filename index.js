f737b248b2f817756175d957de4065344b722293
Dan Wilder <12650041+WilderDan@users.noreply.github.com>
2023-09-30 22:19:44 -0500

    Add submit button handler

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log("here");
+const submitBtn = document.getElementById("submitBtn");
+
+submitBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  validateName();
+  validateEmail();
+  validatePassword();
+});
+
+function validateName() {
+  console.log("name");
+}
+
+function validateEmail() {
+  console.log("email");
+}
+
+function validatePassword() {
+  console.log("password");
+}
