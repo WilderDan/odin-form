1915224fe6f80c2ebd40231a32e5b5eb5070d409
Dan Wilder <12650041+WilderDan@users.noreply.github.com>
2023-10-02 20:28:06 -0500

    Update UI for password mismatch

--- index.js
+++ index.js
@@ -1,14 +1,22 @@
 const submitBtn = document.getElementById("submitBtn");
 submitBtn.addEventListener("click", (e) => {
-  const password = document.getElementById("password").value;
-  const confirmPassword = document.getElementById("confirmPassword").value;
-
-  if (password !== confirmPassword) {
-    console.log("Passwords do not match.");
-    e.preventDefault();
-  }
+  if (!passwordsEqual()) e.preventDefault();
 });
 
 window.addEventListener("submit", (e) => {
   console.log("Form submitted.");
+  e.preventDefault();
 });
+
+window.addEventListener("keyup", (e) => {
+  const passwordErrorMessage = document.getElementById("passwordErrorMessage");
+  passwordErrorMessage.style.visibility = passwordsEqual()
+    ? "hidden"
+    : "visible";
+});
+
+function passwordsEqual() {
+  const passwordValue = document.getElementById("password").value;
+  const confirmPasswordValue = document.getElementById("confirmPassword").value;
+  return passwordValue === confirmPasswordValue;
+}
